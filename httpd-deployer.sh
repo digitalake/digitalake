e7ba62f08b9243fb627d49a36ac668ebea568bf4
digitalake <109740456+digitalake@users.noreply.github.com>
2022-11-19 13:35:41 +0200

    Apache webserver deployment bash script uploaded

---
+++ httpd-deployer.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+lsb_release -is
+if [ -x "$(command -v apk)" ]; then 
+    manager="apk"
+    package="httpd"
+elif [ -x "$(command -v apt)" ]; then 
+    manager="apt"
+    package="apache2"
+elif [ -x "$(command -v yum)" ]; then
+    manager="yum"
+    package="httpd"
+elif [ -x "$(command -v dnf)" ]; then 
+    manager="dnf"
+    package="httpd"
+elif [ -x "$(command -v zypper)" ];  then
+    manager="zypper"
+    package="httpd"
+else echo -e "FAILED TO INSTALL PACKAGE: Package manager or Package not found.";
+fi
+sudo $manager update # apache installation, enabling and status check
+sudo $manager -y install $package
+sudo systemctl start $package
+sudo systemctl enable $package
+sudo chown -R $USER:$USER /var/www
+cd /var/www/html/
+cat << EOF > index.html
+<!DOCTYPE html>
+<html>
+<head>
+<title>GL TEST</title>
+<meta charset="UTF-8">
+</head>
+<body>
+<h3>Yakymenko Ivan</h3>
+</body>
+</html>
+EOF
